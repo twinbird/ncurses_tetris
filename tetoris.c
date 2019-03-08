334c62673f13d5654ad969cab505fd6c7459ce0c
twinbird <ixa2063@gmail.com>
2019-03-08 09:45:39 +0900

    draw block

--- tetoris.c
+++ tetoris.c
@@ -5,6 +5,17 @@
 // フィールドの幅(壁含む)
 #define FIELD_WIDTH (12)
 
+// テトリミノの最大の高さ
+#define TETRIMINO_HEIGHT (4)
+// テトリミノの最大の幅
+#define TETRIMINO_WIDTH (3)
+// テトリミノの種類数
+#define TETRIMINO_KINDS (7)
+
+// 落下位置
+#define FALL_BASE_X (1)
+#define FALL_BASE_Y (0)
+
 // ブロックの種類
 enum blockType {
 	// ブロックがない
@@ -44,6 +55,52 @@ int playField[FIELD_HEIGHT][FIELD_WIDTH] = {
 	{2,2,2,2,2,2,2,2,2,2,2,2}
 };
 
+// テトリミノ
+int tetriminos[TETRIMINO_KINDS][TETRIMINO_HEIGHT][TETRIMINO_WIDTH] = {
+	{
+		{4,0,0},
+		{4,0,0},
+		{4,0,0},
+		{4,0,0}
+	},
+	{
+		{0,4,0},
+		{4,4,4},
+		{0,0,0},
+		{0,0,0}
+	},
+	{
+		{4,4,0},
+		{0,4,4},
+		{0,0,0},
+		{0,0,0}
+	},
+	{
+		{0,4,4},
+		{4,4,0},
+		{0,0,0},
+		{0,0,0}
+	},
+	{
+		{4,4,0},
+		{4,4,0},
+		{0,0,0},
+		{0,0,0}
+	},
+	{
+		{4,4,0},
+		{4,0,0},
+		{4,0,0},
+		{0,0,0}
+	},
+	{
+		{4,4,0},
+		{0,4,0},
+		{0,4,0},
+		{0,0,0}
+	}
+};
+
 // フィールドを描画する
 void drawField() {
 	int h, w;
@@ -57,6 +114,10 @@ void drawField() {
 			if (playField[h][w] == FLOOR) {
 				mvprintw(h, w, "=");
 			}
+			// ブロック
+			if (playField[h][w] == CONTROL) {
+				mvprintw(h, w, "@");
+			}
 		}
 	}
 }
@@ -65,11 +126,18 @@ int main() {
 	// 画面を初期化
 	initscr();
 
+	// とりあえずブロックを描いてみる
+	for (int i = 0; i < TETRIMINO_HEIGHT; i++) {
+		for (int k = 0; k < TETRIMINO_WIDTH; k++) {
+			playField[FALL_BASE_Y + i][FALL_BASE_X + k] = tetriminos[0][i][k];
+		}
+	}
+
 	// フィールドを描画する
 	drawField();
 
 	// qが入力されるまで無限に待つ
-	int ch = NULL;
+	int ch = 0;
 	while (ch != 'q') {
 		ch = getch();
 	}
